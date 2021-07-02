3d227dfe410a0ec0db644a5719f58215f1189387
lemarwin <lemarwin42@gmail.com>
2021-07-02 16:18:51 +0300

    Worker done

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,80 @@
+extern crate bitcoin_utxo;
 extern crate bitcoin;
 extern crate futures;
-extern crate bitcoin_utxo;
-
-use std::net::SocketAddr;
-use std::str::FromStr;use std::sync::Arc;
-
-use bitcoin::OutPoint;
-
-use bitcoin::Script;
-use bitcoin::network::constants;
 
 use bitcoin_utxo::connection::connect;
-
-use futures::pin_mut;
-
-use mempool_filters::filtertree::*;
-
-use mempool_filters::txtree::TxTree;
-
+use bitcoin_utxo::utxo::UtxoState;
+use bitcoin::{BlockHeader,Transaction};
+use bitcoin::consensus::encode::{self, Decodable, Encodable};
+use bitcoin::network::constants;
+use bitcoin::Script;
 use futures::future::{AbortHandle, Abortable};
+use futures::pin_mut;
+use mempool_filters::filtertree::*;
+use mempool_filters::txtree::TxTree;
+use mempool_filters::worker::mempool_worker;
+use std::io;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
-use tokio::time::Duration;
+#[derive(Debug, Clone)]
+pub struct FilterCoin {
+    pub script: Script,
+}
 
-fn foo(o: &OutPoint) -> Result<Script, bitcoin::util::bip158::Error>{
-    match Script::from_str(""){
-        Ok(v) => Ok(v),
-        Err(_) => Err(bitcoin::util::bip158::Error::UtxoMissing(*o))
+impl UtxoState for FilterCoin {
+    fn new_utxo(_height: u32, _header: &BlockHeader, tx: &Transaction, vout: u32) -> Self {
+        FilterCoin {
+            script: tx.output[vout as usize].script_pubkey.clone(),
+        }
     }
+}
+
+impl Encodable for FilterCoin {
+    fn consensus_encode<W: io::Write>(&self, writer: W) -> Result<usize, io::Error> {
+        let len = self.script.consensus_encode(writer)?;
+        Ok(len)
+    }
+}
+impl Decodable for FilterCoin {
+    fn consensus_decode<D: io::Read>(mut d: D) -> Result<Self, encode::Error> {
+        Ok(FilterCoin {
+            script: Decodable::consensus_decode(&mut d)?,
+        })
+    }
+}
+
+fn extract_script(fc: &FilterCoin) -> Script {
+    fc.script.clone()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let address: SocketAddr = "127.0.0.1:8333".parse().unwrap();
+    let dbname = "./db";
+    println!("Opening database {:?}", dbname);
+    let db = Arc::new(bitcoin_utxo::storage::init_storage(dbname.clone(), vec!["filters"])?);
+    println!("Creating cache");
+    let cache = Arc::new(bitcoin_utxo::cache::utxo::new_cache::<FilterCoin>());
 
     let txtree = Arc::new(TxTree::new());
-    let (sync_future, msg_sink, msg_stream) = mempool_filters::worker::sync_mempool(txtree.clone()).await;
+    let ftree = Arc::new(FilterTree::new());
+    let full_filter = Arc::new(Mutex::new(None));
+    let (tx_future, filt_future, msg_sink, msg_stream) = mempool_worker(txtree, ftree, full_filter, db, cache, extract_script).await;
     let (_, abort_server_reg) = AbortHandle::new_pair();
+
     tokio::spawn(async move {
-        tokio::time::sleep(Duration::from_secs(3)).await;
-        let res = sync_future.await;
-        match res {
-            Ok(_) => println!("DONE"),
-            Err(e) => eprintln!("ERROR: {:?}", e)
-        }
+        let res = tx_future.await;
+        res.map_or_else(|e| eprintln!("ERROR: {:?}", e), |_| println!("DONE"))
     });
+
     tokio::spawn(async move {
-        let filt_tree = FilterTree::new();
-        loop {
-            let txtree = txtree.clone();
-            tokio::time::sleep(Duration::from_secs(30)).await;
-            make_filters(&filt_tree, &txtree, foo);
-            let full_filter = make_full_filter(&txtree, foo).unwrap();
-            println!("{:?}", full_filter);
-        }
+        let res = filt_future.await;
+        res.map_or_else(|e| eprintln!("ERROR: {:?}", e), |_| println!("DONE"))
     });
+
     pin_mut!(msg_sink);
 
     let res = Abortable::new(connect(
--- src/worker.rs
+++ src/worker.rs
@@ -1,46 +1,125 @@
-use std::sync::Arc;
+use bitcoin_utxo::cache::utxo::UtxoCache;
+use bitcoin_utxo::cache::utxo::wait_utxo_noh;
+use bitcoin::consensus::Decodable;
 use bitcoin::consensus::encode;
+use bitcoin::network::message_blockdata::Inventory;
 use bitcoin::network::message::NetworkMessage;
+use bitcoin::OutPoint;
+use bitcoin::Script;
+use bitcoin::util::bip158;
+use ergvein_filters::mempool::ErgveinMempoolFilter;
 use futures::Future;
-use tokio::sync::mpsc;
-use tokio::sync::broadcast;
+use futures::sink;
+use rocksdb::DB;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
 use tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio::sync::broadcast;
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 
 use crate::error::MempoolErrors;
-
+use crate::filtertree::FilterTree;
+use crate::filtertree::make_filters;
+use crate::filtertree::make_full_filter;
+use crate::txtree::insert_tx;
 use crate::txtree::TxTree;
 
-use crate::txtree::insert_tx;use futures::sink;
-use bitcoin::network::message_blockdata::Inventory;
-
-pub async fn sync_mempool(
+pub async fn mempool_worker<T, M>(
     txtree: Arc<TxTree>,
+    ftree: Arc<FilterTree>,
+    full_filter: Arc<Mutex<Option<ErgveinMempoolFilter>>>,
+    db: Arc<DB>,
+    cache: Arc<UtxoCache<T>>,
+    script_from_t : M,
 ) -> (
+    impl Future<Output = Result<(), MempoolErrors>>,
     impl Future<Output = Result<(), MempoolErrors>>,
     impl futures::Sink<NetworkMessage, Error = encode::Error>,
     impl Unpin + futures::Stream<Item = NetworkMessage>
 )
+where
+T:Decodable + Clone,
+M:Fn(&T) -> Script + Copy
 {
     const BUFFER_SIZE: usize = 100;
     let (broad_sender, _) = broadcast::channel(BUFFER_SIZE);
     let (msg_sender, msg_reciver) = mpsc::unbounded_channel::<NetworkMessage>();
-    let sync_future = {
+    let tx_future = {
         let broad_sender = broad_sender.clone();
+        let txtree = txtree.clone();
         async move {
             loop {
-                mempool_worker(txtree.clone(), &broad_sender, &msg_sender).await?;
+                tx_listener(txtree.clone(), &broad_sender, &msg_sender).await?;
             }
         }
     };
+    let filt_future = filter_worker(
+            txtree.clone(),
+            ftree.clone(),
+            full_filter.clone(),
+            db.clone(),
+            cache.clone(),
+            script_from_t);
+
     let msg_stream = UnboundedReceiverStream::new(msg_reciver);
     let msg_sink = sink::unfold(broad_sender, |broad_sender, msg| async move {
         broad_sender.send(msg).unwrap_or(0);
         Ok::<_, encode::Error>(broad_sender)
     });
-    (sync_future, msg_sink, msg_stream)
+    (tx_future, filt_future, msg_sink, msg_stream)
 }
 
-pub async fn mempool_worker(
+pub async fn filter_worker<T, M>(
+    txtree: Arc<TxTree>,
+    ftree: Arc<FilterTree>,
+    full_filter: Arc<Mutex<Option<ErgveinMempoolFilter>>>,
+    db: Arc<DB>,
+    cache: Arc<UtxoCache<T>>,
+    script_from_t : M,
+) -> Result<(), MempoolErrors>
+where
+    T: Decodable + Clone,
+    M: Fn(&T) -> Script,
+{
+    let mut hashmap = HashMap::<OutPoint, Script>::new();
+    loop {
+        tokio::time::sleep(Duration::from_secs(30)).await;
+        hashmap.clear();
+        for txs in txtree.iter() {
+            for (_, tx) in txs.value().iter(){
+                if !tx.is_coin_base(){
+                    for i in &tx.input {
+                        let coin = wait_utxo_noh(
+                            db.clone(),
+                            cache.clone(),
+                            &i.previous_output,
+                            Duration::from_millis(100),
+                        ).await;
+                        coin.map_or((), |t|{
+                            hashmap.insert(i.previous_output, script_from_t(&t));
+                        })
+                    }
+                }
+            }
+        };
+        make_filters(&ftree, &txtree, |out| {
+            hashmap.get(out).map_or( Err(bip158::Error::UtxoMissing(*out)), |s| Ok(s.clone()))
+        });
+
+        let ff = make_full_filter(&txtree, |out| {
+            hashmap.get(out).map_or( Err(bip158::Error::UtxoMissing(*out)), |s| Ok(s.clone()))
+        }).map_err(|e| eprintln!("Error making the full filter! {:?}", e)).ok();
+        {
+            let mut ffref = full_filter.lock().await;
+            *ffref = ff;
+            println!("Filter: {:?}", ffref);
+        }
+    };
+}
+
+pub async fn tx_listener(
     txtree: Arc<TxTree>,
     broad_sender: &broadcast::Sender<NetworkMessage>,
     msg_sender: &mpsc::UnboundedSender<NetworkMessage>,
@@ -66,7 +145,7 @@ pub async fn mempool_worker(
                     }
                     _ => ()
                 },
-                Err(e) => eprintln!("mempool_worker: {:?}", e),
+                Err(e) => eprintln!("tx_listener: {:?}", e),
             }
         }
     };
