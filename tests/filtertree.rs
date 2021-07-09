22eba0bb0945c7a3cc91302a6a37e287c3bf2b77
lemarwin <lemarwin42@gmail.com>
2021-07-09 22:46:03 +0300

    Add tests

---
+++ tests/filtertree.rs
@@ -0,0 +1,61 @@
+use bitcoin::{
+    consensus::deserialize,
+    hashes::hex::FromHex,
+    OutPoint, Script, Transaction
+};
+use ergvein_filters::util::is_script_indexable;
+use mempool_filters::filtertree::*;
+use mempool_filters::txtree::*;
+use std::io;
+use std::io::BufRead;
+
+#[test]
+fn full_filter(){
+    let mut txtree = TxTree::new();
+    let txs = load_txs("./tests/block1-txs");
+    insert_tx_batch(&mut txtree, txs.clone());
+    let filter = make_full_filter(&txtree, emptyscripts).expect("Failed to match tx! ");
+    let (k0,k1) = get_full_prefix();
+    for tx in txs {
+        let is_indexable = tx.output.iter().any(|o| is_script_indexable(&o.script_pubkey));
+        if is_indexable {
+            let b = filter.match_tx_outputs(k0, k1, &tx).expect("Filter matching error. ");
+            assert!(b, "Failed to match tx! It didn't match when it should");
+        }
+    };
+}
+
+#[test]
+fn bucket_filters(){
+    let mut txtree = TxTree::new();
+    let txs = load_txs("./tests/block1-txs");
+    insert_tx_batch(&mut txtree, txs.clone());
+
+    let ftree = FilterTree::new();
+    make_filters(&ftree, &txtree, emptyscripts);
+
+    for tx in txs.iter(){
+        let is_indexable = tx.output.iter().any(|o| is_script_indexable(&o.script_pubkey));
+        if is_indexable {
+            let pref = make_prefix(&tx.txid());
+            let filter = ftree.get(&pref).expect("Failed to get a filter");
+            let (k0, k1) = mk_siphash_keys(&pref);
+            let b = filter.match_tx_outputs(k0, k1, &tx).expect("Filter matching error. ");
+            assert!(b, "Failed to match tx! It didn't match when it should");
+        };
+    }
+}
+
+fn load_txs(path: &str) -> Vec<Transaction> {
+    let mut res = vec![];
+    let file = std::fs::File::open(path).unwrap();
+    for line in io::BufReader::new(file).lines() {
+        let tx = deserialize(&Vec::from_hex(&line.unwrap()).unwrap()).unwrap();
+        res.push(tx);
+    }
+    res
+}
+
+fn emptyscripts(_: &OutPoint) -> Result<Script, bitcoin::util::bip158::Error>{
+    Ok(Script::new())
+}
---
+++ tests/txtree.rs
@@ -0,0 +1,62 @@
+use bitcoin::{
+    consensus::deserialize,
+    hashes::hex::FromHex,
+    Transaction
+};
+use mempool_filters::txtree::*;
+use std::io;
+use std::io::BufRead;
+
+#[test]
+fn correct_inputs(){
+    let mut txtree = TxTree::new();
+    let txs = load_txs("./tests/block1-txs");
+    insert_tx_batch(&mut txtree, txs.clone());
+    for tx in txs.iter() {
+        let txid = &tx.txid();
+        let pref = make_prefix(txid);
+        let txs = txtree.get(&pref).expect("Failed to get tx bucket!");
+        let tx2 = txs.get(txid).expect("Failed to get tx from bucket!");
+        assert_eq!(tx, tx2);
+    }
+}
+
+#[test]
+fn correct_removal() {
+    let mut txtree = TxTree::new();
+    let txs = load_txs("./tests/block1-txs");
+    insert_tx_batch(&mut txtree, txs.clone());
+    let (todel, tokeep) = txs.split_at(10);
+    remove_batch(&txtree, &todel.to_vec());
+    for tx in todel.iter() {
+        let txid = &tx.txid();
+        let pref = make_prefix(txid);
+        match txtree.get(&pref){
+            // If it's the only tx with the prefix, the whole bucked is removed
+            // So we have to check that
+            None => assert_eq!(count_with_prefix(pref, &txs),1),
+            Some(bucket) => assert_eq!(None, bucket.get(txid))
+        }
+    }
+    for tx in tokeep.iter(){
+        let txid = &tx.txid();
+        let pref = make_prefix(txid);
+        let txs = txtree.get(&pref).expect("Failed to get tx bucket!");
+        let tx2 = txs.get(txid).expect("Failed to get tx from bucket!");
+        assert_eq!(tx, tx2);
+    }
+}
+
+fn count_with_prefix(pref: TxPrefix, txs: &Vec<Transaction>) -> usize{
+    txs.iter().filter(|tx| pref == make_prefix(&tx.txid())).count()
+}
+
+fn load_txs(path: &str) -> Vec<Transaction> {
+    let mut res = vec![];
+    let file = std::fs::File::open(path).unwrap();
+    for line in io::BufReader::new(file).lines() {
+        let tx = deserialize(&Vec::from_hex(&line.unwrap()).unwrap()).unwrap();
+        res.push(tx);
+    }
+    res
+}
